4d984723642a2b5deeee000f56586694090cc85c
William Knight <109128542+wknight0@users.noreply.github.com>
2023-03-14 12:48:24 +1300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Home } from './components';
+
+function App() {
+  return (
+    <div className="App overflow-hidden text-white">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
